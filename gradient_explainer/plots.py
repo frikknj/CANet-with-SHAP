634a18cddf6659f85e764b591e447cb6852a826b
frikknj <frikk.nj@gmail.com>
2024-07-28 10:53:51 +0200

    Separate file into value generating and plotting

--- gradient_explainer/plots.py
+++ gradient_explainer/plots.py
@@ -1,10 +1,45 @@
 # Run script with:
 #python3 -m gradient_explainer.plots
+# Alternatively:
+#python3 -m gradient_explainer.gradient_explainer; python3 -m gradient_explainer.plots
 
+import shap
 import joblib
+import matplotlib.pyplot as plt
+import cv2
+import os
+import numpy as np
+
+def scale(image):
+    image_cropped = image[0:1488,376:1864]
+    image_resized = cv2.resize(image_cropped, (224,224))
+    return image_resized
+
+def scale_all(images):
+    images_scaled = []
+    for image in images:
+        image_scaled = scale(image)
+        images_scaled.append(image_scaled)
+    return images_scaled
+
+def make_list_images(directory_path):
+    images_list = []
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".tif"):
+            image_path = os.path.join(directory_path, filename)
+            image = cv2.imread(image_path)
+            images_list.append(image)
+    return images_list
+
+directory_path = "missdor/Base11"
+
+X = make_list_images(directory_path)
+X = np.array(scale_all(X))
+
+to_explain = X[[38,39,40]]
 
 shap_values = joblib.load('gradient_explainer/shap_values.joblib')
 
-shap.image_plot(shap_values, to_explain)#, indexes)
+shap.image_plot(shap_values, to_explain)
 plt.savefig(f'gradient_explainer/images/image_plot_2.png', bbox_inches='tight', pad_inches=0)
 plt.close()
